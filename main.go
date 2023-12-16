f86e946af51edd864ceef46cacf26da54320858a
krishna-pramod-aradhi <krishna-pramod.aradhi@hpe.com>
2023-12-16 19:38:20 +0530

    Added code for accepting input and output paths

--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type Product struct {
+	Id               string  `json:"id"`
+	Title            string  `json:"title"`
+	ShortDescription string  `json:"shortDescription"`
+	Description      string  `json:"description"`
+	Category         string  `json:"category"`
+	Price            float64 `json:"price"`
+	Quantity         int     `json:"quantity"`
+	ImageUrl         string  `json:"imageUrl"`
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Path to CSV file: ")
+	inputPath, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("An issue occured while reading the path", err)
+		return
+	}
+	inputPath = strings.TrimSuffix(inputPath, "\n")
+	inputPath = strings.TrimSuffix(inputPath, "\r")
+	outputPath, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("An issue occured while reading the path", err)
+		return
+	}
+	outputPath = strings.TrimSuffix(inputPath, "\n")
+	outputPath = strings.TrimSuffix(inputPath, "\r")
+	fmt.Println(inputPath, outputPath)
+	validPath := regexp.MustCompile(`^(?:[\w]\:|\\)(\\[a-zA-Z_\-\s0-9\.]+)+\.(csv)`)
+	match := validPath.MatchString(inputPath)
+	if !match {
+		fmt.Println("The input path is invalid")
+		return
+	}
+
+	file, err := os.Open(inputPath)
+	if err != nil {
+		fmt.Println("An error occured while reading the file", err)
+		return
+	}
+	defer file.Close()
+
+	products := make([]Product, 0, 20)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		row := scanner.Text()
+	}
 }
